e885002f43e9f1cb908ea0d2dc7c098b492ad4e3
calloc134 <115209243+calloc134@users.noreply.github.com>
2023-04-03 20:40:33 +0900

    [update]　非同期で正常に動作するように改良

--- src/utils.rs
+++ src/utils.rs
@@ -1,11 +1,11 @@
 use regex::Regex;
-use reqwest::{Client, Error, Response};
+use reqwest::{Client, Error, Response, StatusCode};
 use std::fs::File;
 use std::io::BufReader;
 use std::{collections::HashMap, str::FromStr};
 use tokio::task::JoinHandle;
 
-use serde_json::Value;
+use serde_json::{to_string_pretty, Value};
 
 mod types;
 use types::{InitStep, JsonMap, TestConfig, TestStep};
@@ -13,16 +13,19 @@ use types::{InitStep, JsonMap, TestConfig, TestStep};
 // テスト構成ファイルの構造体を生成する関数
 pub fn gen_struct(index_path: String) -> Result<(TestConfig, JsonMap), Box<dyn std::error::Error>> {
     // テスト構成ファイルを読み込む
+    println!("[*] Loading test config file...");
     let config_file = File::open(&index_path)?;
     let reader = BufReader::new(config_file);
     let test_config: TestConfig = serde_yaml::from_reader(reader)?;
 
     // データファイルのパス指定が正しいかチェックする
+    println!("[*] Checking data file path...");
     if !test_config.data.starts_with("json://") {
         return Err("Invalid data file path".into());
     }
 
     // データの格納されているjsonファイルを読み込む
+    println!("[*] Loading json data file...");
     let data_file = File::open(test_config.data.trim_start_matches("json://"))?;
     let reader = BufReader::new(data_file);
     let json_data: JsonMap = serde_json::from_reader(reader)?;
@@ -48,6 +51,7 @@ pub async fn run_init(
         });
 
     // HTTPクライアントを初期化
+    println!("[*] Initializing HTTP client...");
     let client = Client::new();
 
     let tasks: Vec<JoinHandle<Result<Response, Error>>> = init
@@ -58,6 +62,7 @@ pub async fn run_init(
 
             // アクセスするURLを作成する
             let url = format!("{}/{}", base_url, init_step.path);
+            println!("[*] Accessing {}...", url);
 
             // リクエストクライアントの作成
             let mut request = client_clone.request(
@@ -68,6 +73,10 @@ pub async fn run_init(
             // リクエストボディがある場合は、jsonデータよりリクエストボディを設定する
             if let Some(body) = &init_step.body {
                 let init_data = json_data.get(body).unwrap().get("body").unwrap();
+                println!(
+                    "[*] Request body: {}",
+                    to_string_pretty(&init_data).unwrap()
+                );
                 request = request.json(&init_data);
             }
 
@@ -83,14 +92,16 @@ pub async fn run_init(
 
         // クッキーをハッシュマップに格納する
         if let Some(value) = cookie_map.get_mut(init[i].name.as_str()) {
-            *value = response
-                .headers()
-                .get("set-cookie")
-                .unwrap()
-                .to_str()
-                .unwrap()
-                .to_string();
+            let header = &response.headers().get("set-cookie").unwrap();
+            *value = header.to_str().unwrap().to_string();
+
+            println!("[b] Cookie: {}", value);
         }
+        // レスポンスボディを表示する
+        let body = response.text().await?;
+        println!("[b] Response body: {}", body);
+        println!("[b] Init step {} completed", init[i].name);
+        println!("")
     }
 
     Ok(cookie_map)
@@ -106,15 +117,15 @@ pub async fn run_test(
     // HTTPクライアントを初期化
     let client = Client::new();
 
-    let tasks: Vec<JoinHandle<Result<Response, Error>>> = steps
+    let tasks: Vec<JoinHandle<Result<(usize, Response), Error>>> = steps
         .iter()
-        .map(|test_step| {
+        .enumerate()
+        .map(|(index, test_step)| {
             let client_clone = client.clone();
 
             // クエリの指定がある場合は、jsonデータよりクエリを設定し、URLを書き換える
             let rewrite_path = if let Some(query) = &test_step.query {
                 let test_query = &json_data.get(query).unwrap().get("query").unwrap();
-                println!("query -> {:?}", test_query);
 
                 // 正規表現をコンパイル
                 let re = Regex::new(r"\{(\w+)\}").unwrap();
@@ -129,7 +140,7 @@ pub async fn run_test(
                         }
                     });
 
-                println!("replaced -> {:?}", replaced_string);
+                println!("[*] Query: {}", to_string_pretty(&test_query).unwrap());
                 replaced_string.to_string()
             // クエリの指定がない場合は、パスをそのまま使用する
             } else {
@@ -138,8 +149,7 @@ pub async fn run_test(
 
             // アクセスするURLを作成する
             let url = format!("{}/{}", base_url, rewrite_path);
-
-            println!("url -> {:?}", url);
+            println!("[*] Accessing {}...", url);
 
             // リクエストクライアントの作成
             let mut request = client_clone.request(
@@ -151,8 +161,11 @@ pub async fn run_test(
             // リクエストボディがある場合は、jsonデータよりリクエストボディを設定する
             if let Some(body) = &test_step.body {
                 let test_body = &json_data.get(body).unwrap().get("body").unwrap();
-                println!("body -> {:?}", test_body);
                 request = request.json(test_body);
+                println!(
+                    "[*] Request body: {}",
+                    to_string_pretty(&test_body).unwrap()
+                );
             }
 
             // ログインが必要な場合は、クッキーを設定する
@@ -160,95 +173,42 @@ pub async fn run_test(
                 println!("hogehoge Password required");
 
                 let cookie = cookie_map.get(login).unwrap();
-                println!("cookie -> {:?}", cookie);
+                println!("[*] Cookie: {}", cookie);
 
                 request = request.header("Cookie", cookie);
             }
 
-            tokio::spawn(async move { return request.send().await })
+            tokio::spawn(async move {
+                return match request.send().await {
+                    Ok(response) => Ok((index, response)),
+                    Err(e) => Err(e.into()),
+                };
+            })
         })
         .collect();
 
     for task in tasks {
-        let response = task.await??;
+        let (index, response) = task.await??;
+        let status = response.status();
 
-        println!("status -> {:?}", response.status());
-        println!("headers -> {:?}", response.headers());
-        println!("text -> {:?}", response.text().await?);
-    }
+        println!("[*] Status: {}", status);
+        println!("[*] Headers: {:?}", response.headers());
+        println!("[*] Response body: {}", response.text().await?);
 
-    // テストステップを実行する
-    /*
-    for test_step in steps {
-        let client_clone = client.clone();
-
-        // クエリの指定がある場合は、jsonデータよりクエリを設定し、URLを書き換える
-        let rewrite_path = if let Some(query) = test_step.query {
-            let test_query = &json_data.get(&query).unwrap().get("query").unwrap();
-            println!("query -> {:?}", test_query);
-
-            // 正規表現をコンパイル
-            let re = Regex::new(r"\{(\w+)\}").unwrap();
-            let replaced_string =
-                re.replace_all(test_step.path.as_str(), |captures: &regex::Captures| {
-                    let key = &captures[1];
-                    let query_original = test_query.get(key).unwrap();
-                    match query_original {
-                        Value::String(s) => s.clone(),
-                        Value::Number(n) => n.to_string(),
-                        _ => "".to_string(),
-                    }
-                });
-
-            println!("replaced -> {:?}", replaced_string);
-            replaced_string.to_string()
-        // クエリの指定がない場合は、パスをそのまま使用する
+        if status == steps[index].expect_status {
+            println!("[b] Test passed!");
+        } else if status == StatusCode::OK {
+            println!("[b] Test failed, but status is 200 OK");
         } else {
-            test_step.path.clone()
-        };
-
-        // アクセスするURLを作成する
-        let url = format!("{}/{}", base_url, rewrite_path);
-
-        println!("url -> {:?}", url);
-
-        // リクエストクライアントの作成
-        let mut request = client.request(
-            reqwest::Method::from_bytes(test_step.method.as_bytes())?,
-            &url,
-        );
-
-        // リクエストボディがある場合は、jsonデータよりリクエストボディを設定する
-        if let Some(body) = test_step.body {
-            let test_body = &json_data.get(&body).unwrap().get("body").unwrap();
-            println!("body -> {:?}", test_body);
-            request = request.json(test_body);
+            println!(
+                "[!] Test failed! (status: {}, expect status: {})",
+                status, steps[index].expect_status
+            );
         }
 
-        // ログインが必要な場合は、クッキーを設定する
-        if let Some(login) = test_step.login {
-            println!("hogehoge Password required");
-
-            let cookie = cookie_map.get(&login).unwrap();
-            println!("cookie -> {:?}", cookie);
-
-            request = request.header("Cookie", cookie);
-        }
-
-        let response = request.send()?;
-
-        println!("text -> {:?}", response.text()?);
-
-
-        if response.status() != test_step.expect_status {
-            println!("Error: status code is not expected");
-        } else if response.status() == test_step.expect_status {
-            println!("Success: status code is expected");
-        } else {
-            return Err("Error: 予期せぬエラー".into());
-        }
+        println!("[b] Test step {} completed", steps[index].name);
+        println!("")
     }
-    */
 
     Ok(())
 }
