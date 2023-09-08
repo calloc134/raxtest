a93c0722109558910cc9b6b49d19b3103c5f0fe0
calloc134 <115209243+calloc134@users.noreply.github.com>
2023-09-08 18:48:51 +0900

    fix: サブタイトルの修正

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -7,7 +7,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 /** @type {import('@docusaurus/types').Config} */
 const config = {
   title: 'RaxTest: Asynchronous API Testing Tool in Rust',
-  tagline: 'Subtitle: Light-Weight, Fast, and Extensible',
+  tagline: 'Light-Weight, Fast, and Extensible',
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
